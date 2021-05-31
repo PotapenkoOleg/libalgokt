14b33022f03812d947f72eda86c327f5be29acdf
Oleg Potapenko <PotapenkoOleg@gmail.com>
2021-05-31 03:15:40 -0400

    Hashtables starter

---
+++ libalgokt/src/main/kotlin/com/libalgokt/hashtables/separatechaining/HashTableSeparateChaining.kt
@@ -0,0 +1,30 @@
+package com.libalgokt.hashtables.separatechaining
+
+import com.libalgokt.common.interfaces.HashTable
+
+class HashTableSeparateChaining<Key, Value> : HashTable<Key, Value> {
+
+    override fun add(key: Key, value: Value) {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(key: Key): Value? {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(key: Key): Value? {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSize(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ libalgokt/src/test/kotlin/com/libalgokt/hashtables/separatechaining/HashTableSeparateChainingTest.kt
@@ -0,0 +1,42 @@
+package com.libalgokt.hashtables.separatechaining
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HashTableSeparateChainingTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun remove() {
+    }
+
+    @Test
+    fun isEmpty() {
+    }
+
+    @Test
+    fun clear() {
+    }
+
+    @Test
+    fun getSize() {
+    }
+}
